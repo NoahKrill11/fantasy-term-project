9adc873ab87342e71ac79855ead55cad6734c30c
Noah Krill <noahkrill@Noahs-MacBook-Pro.local>
2020-11-11 21:55:20 -0500

    finish serial average function

--- fantasy.c
+++ fantasy.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 int size=0;
-void average(double ar[]);
+void serialAverage(double ar[]);
 int main(void)
 {
 	double fantasyPoints[]= {14.4,15.3,15.8,15.8,16.4,16.8,16.8,17.4,17.6,17.7,17.9,18.3,
 							18.4,19.4,19.5,19.6,19.7,20,20.1,20.2,20.6,20.9,20.9,21,21.2,
 							21.5,21.7,21.7,23.4,26.3,27.6};
 	size = sizeof(fantasyPoints)/sizeof(fantasyPoints[0]);
-	average(fantasyPoints);
+	serialAverage(fantasyPoints);
 
 
 }
-void average(double fantasyPoints[])
+void serialAverage(double fantasyPoints[])
 {
-	
+	double sum=0;
+	for(int i=0; i<size; ++i)
+	{
+		sum+=fantasyPoints[i];
+	}
+	double avg=sum/size;
+	printf("%s", "The average for the fantasy teams is " );
+	printf("%f\n",avg);
 
 }
